cfd544e518765f0329dbaf27f6225cefc086c5f5
Samir Mahmud <samir93bj@gmail.com>
2023-01-23 22:00:09 -0300

    refactor: add fillable columns and column migration to model Sale

--- database/migrations/2023_01_24_005541_create_sales_table.php
+++ database/migrations/2023_01_24_005541_create_sales_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total', 10,2);
+            $table->integer('items');
+            $table->decimal('cash', 10,2);
+            $table->decimal('change', 10,2);
+            $table->enum('status',['PAID','PENDING', 'CANCELLED'])->default('PAID');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
