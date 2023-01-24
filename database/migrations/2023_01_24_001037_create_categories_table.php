31fc1424973bae983c16c81feea571e56649cc7d
Samir Mahmud <samir93bj@gmail.com>
2023-01-23 21:13:06 -0300

    refactor: add fillable columns and column migration to model Category

--- database/migrations/2023_01_24_001037_create_categories_table.php
+++ database/migrations/2023_01_24_001037_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('image', 100);
             $table->timestamps();
         });
     }
