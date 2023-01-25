510ff816ab583c3485d183f12020e3e0af3daac6
Samir Mahmud <samir93bj@gmail.com>
2023-01-24 21:16:38 -0300

    refactor: increment large of string en value password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->enum('profile', ['ADMIN', 'EMPLOYEE'])->default('ADMIN');
             $table->enum('status', ['ACTIVE', 'LOCKED'])->default('ACTIVE');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 50);
+            $table->string('password', 100);
             $table->string('image', 50)->nullable();
             $table->rememberToken();
             $table->timestamps();
