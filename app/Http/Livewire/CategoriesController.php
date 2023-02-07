731d5dcf0b311fe4577bff73452ef0a658c17e71
Samir Mahmud <samir93bj@gmail.com>
2023-02-07 19:22:10 -0300

    chore: get categories and return in vew common categorie

---
+++ app/Http/Livewire/CategoriesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Storage;
+
+class CategoriesController extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    private $pagination = 5;
+
+    public function render()
+    {
+        return view('livewire.category.categories')->extends('layouts.theme.app')->section('content');
+    }
+}
